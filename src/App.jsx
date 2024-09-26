786023c4bf07ecb8163f3eb5e1b721663644b43e
Rayen Souissi <rayensouissi18@gmail.com>
2024-09-26 13:51:40 +0100

    fix deploy errror

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./pages/Header";
+import Header from "./pages/header";
 // import Footer from "./pages/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
