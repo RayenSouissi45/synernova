f8b8063ceeee3812395a1983a62f024853902093
Rayen Souissi <rayensouissi18@gmail.com>
2024-10-01 13:26:56 +0100

    add image and description

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ogImage from "./assets/images/footersynernova.png";
 
-createRoot(document.getElementById('root')).render(
+// Set the Open Graph image dynamically
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector('meta[property="og:image"]')
+    .setAttribute("content", ogImage);
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
